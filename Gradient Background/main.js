cdc8d9aaa62ef31964d7c8f5c86876c458aea636
moheb-gamil-nafea <mohebgamil1@gmail.com>
2024-08-06 20:10:13 +0200

    Updated Tasks

--- Gradient Background/main.js
+++ Gradient Background/main.js
@@ -1,10 +1,10 @@
 let input1 = document.querySelector('.input1'),
     input2 = document.querySelector('.input2'),
-    body = document.querySelector('body')
+    body = document.querySelector('#gradientcolor')
         
       function SetUpBackground(){
-        body.Style.background =
-        `linear-gradient(to right, ${input1.value}, ${input2.value})`
+        body.style.background ='linear-gradient(to right, ${input1.value}, ${input2.value})';
       }
+      
     input1.addEventListener('input', SetUpBackground);
     input2.addEventListener('input', SetUpBackground);
